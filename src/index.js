6281571e2b8dbbf16427eb3e902027213b6959b9
Cody Reichert <cody@reichertbrothers.com>
2018-06-09 16:09:38 -0500

    Start logging after calling next action
    When the logging group starts before calling next(), logging from
    other parts of the app may get caught in the group and not be
    immediately visible. This logs everything at once after calling the
    next action so that the group closes immediately

--- src/index.js
+++ src/index.js
@@ -6,19 +6,17 @@ export default ({ getState }) => next => action => {
         return next(action)
     }
 
-    console.groupCollapsed(
-        `%c${action.type}`,
-        "font-weight:bold;font-size:14;color:rgb(23, 162, 184)"
-    )
-
     const previousState = getState()
     const result = next(action)
     const nextState = getState()
 
+    console.groupCollapsed(
+        `%c${action.type}`,
+        "font-weight:bold;font-size:14;color:rgb(23, 162, 184)"
+    )
     console.log("%cPrevious state: ", "font-weight:bold", previousState)
     console.log("%cAction: ", "font-weight:bold", action);
     console.log("%cNext state: ", "font-weight:bold", nextState)
-
     console.groupEnd()
 
     return result
